5447307409d4c994fe54282fc018b7f84335e1d3
sujal0786 <sujal6067@gmail.com>
2025-05-04 19:01:30 +0530

    Arrays

--- Arrays/MaximumSubarraySum.java
+++ Arrays/MaximumSubarraySum.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
-        int maxi=nums[0];
-        int sum=0;
-        for(int i = 0 ; i< nums.length ; i++){
-            sum+=nums[i];
-            maxi=Math.max(maxi,sum);
-            if(sum<0){
-                sum=0;
-            }
+        int maxi = nums[0];
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+nums[i];
+            maxi = maxi<sum?sum:maxi;
+            if(sum<0) sum = 0;
         }
         return maxi;
     }
