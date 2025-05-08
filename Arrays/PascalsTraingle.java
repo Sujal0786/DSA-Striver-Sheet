438839652d12c90749d47300b3f85550d1b8f05d
sujal0786 <sujal6067@gmail.com>
2025-05-08 15:11:35 +0530

    Arrays

---
+++ Arrays/PascalsTraingle.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>();
+            long value = 1;
+
+            for (int col = 0; col <= row; col++) {
+                currentRow.add((int) value);
+                value = value * (row - col) / (col + 1);
+            }
+
+            triangle.add(currentRow);
+        }
+
+        return triangle;
+    }
+}
