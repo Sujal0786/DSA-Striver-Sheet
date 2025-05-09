11d831505ac9911d5aa77c728511819d5f8293b1
sujal0786 <sujal6067@gmail.com>
2025-05-09 19:45:39 +0530

    Arrays

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -1,17 +1,15 @@
-public import java.util.*;
-
 public class ThreeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums);  // Step 1: Sort the array
+        Arrays.sort(nums);  
 
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicates for the first number
+        
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;              // Step 2: left pointer
-            int right = nums.length - 1;   // Step 3: right pointer
+            int left = i + 1;              
+            int right = nums.length - 1;   
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
@@ -19,16 +17,16 @@ public class ThreeSum {
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // Skip duplicates for left and right
+                  
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
                 } else if (sum < 0) {
-                    left++; // Increase sum
+                    left++; 
                 } else {
-                    right--; // Decrease sum
+                    right--; 
                 }
             }
         }
